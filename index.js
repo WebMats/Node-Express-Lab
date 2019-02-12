a7b9d1d5d94c5ef74621ef74e0463bd54e57a073
WebMats <lorlando127@gmail.com>
2019-02-12 18:26:47 -0500

    added DELETE and PUT routes

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 
 app.use(express.json());
-
+app.use(cors());
 const postRoutes = require('./routes/posts');
 
 app.use('/api/posts', postRoutes);
